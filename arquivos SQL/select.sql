3a3d48716c89e341dab357f28d53e80910416dce
Mellyssa Stephanny <31068578+mellyssaStephanny@users.noreply.github.com>
2017-11-08 01:26:57 -0200

    Add files via upload

---
+++ arquivos SQL/select.sql
@@ -0,0 +1,10 @@
+select * from aluno;
+select * from curso;
+select * from documento;
+select * from municipio;
+select * from orgao_emissor;
+select * from rg;
+select * from tipo_documento;
+select * from titulo_eleitor;
+select * from uf;
+
