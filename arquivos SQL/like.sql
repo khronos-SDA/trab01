2f8943b092382b052732347461b88fd219e2f2f8
Mellyssa Stephanny <31068578+mellyssaStephanny@users.noreply.github.com>
2017-11-08 04:14:40 -0200

    Add files via upload

---
+++ arquivos SQL/like.sql
@@ -0,0 +1,8 @@
+/* Like */
+
+/*Consulta 9.4 */
+select * from aluno where nome like 'V%';
+select * from rg where nome_pai like 'J%';
+select * from municipio where nome_municipio like 'M%';
+select * from rg where nome_mae like 'L%';
+
